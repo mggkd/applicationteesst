8dcd7f974bb456691cce5448e77a7cac5e40257f
mggkd <106995135+mggkd@users.noreply.github.com>
2022-12-05 19:53:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const port = 13000;
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end('<html><body><h1>Hello, World! Bro</h1></body></html>');
+ res.end('<html><body><h1>Hello, World! Bro how are you fine good</h1></body></html>');
 })
 server.listen(port, hostname);
